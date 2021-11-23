52cfd925f4247603aac52aa9338f3e8685bd15fa
Fran Naranjo <fjnargal@gmail.com>
2021-11-23 13:49:02 +0100

    changes

--- set_lengths.py
+++ set_lengths.py
@@ -1,12 +1,17 @@
 import sys
 
+
+
 with open(sys.argv[1], "r") as fh:
     lines = fh.readlines()
 
 for line in lines:
+    # remove newlines
     line = line.strip()
+    # Header line starts with ">"
     if line.startswith(">"):
         header = line
     else: 
         length = len(line)
+        
         print(length, header)
